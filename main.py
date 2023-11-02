6124a0e8b42ba0fd07582ddb58a7a575388862b3
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-02 14:43:54 -0300

    basic binary tree structure

---
+++ main.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, element):
+        self.element = element
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+
+
