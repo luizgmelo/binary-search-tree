708ed38d65f464e1d04c4b527e1be7fd24f1d95f
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-02 14:46:41 -0300

    new: created insert nodes method

--- main.py
+++ main.py
@@ -9,6 +9,30 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
 
+    def insert(self, element):
+        return self.__insertNode(element, self.root)
+
+    def __insertNode(self, element, node):
+        new_node = Node(element)
+        if (self.root is None):
+            self.root = new_node
+            return
+
+        while (node is not None):
+            if (node.element > new_node.element):
+                if (node.left is None):
+                    node.left = new_node
+                    return
+                return self.insertNode(element, node.left)
+            if (node.element < new_node.element):
+                if (node.right is None):
+                    node.right = new_node
+                    return
+                return self.insertNode(element, node.right)
 
 
+tree = BinarySearchTree()
+tree.insert(11)
+tree.insert(13)
+tree.insert(10)
 
